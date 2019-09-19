4d38131146d7f0dd4622df3e8d8721b5e0574421
qianxiaweiliang <zelanwang@126.com>
2019-09-19 16:26:08 +0800

    test init

---
+++ src/main/java/com/base/project/controller/BaseController.java
@@ -0,0 +1,8 @@
+package com.base.project.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BaseController {
+    private static final Logger logger = LoggerFactory.getLogger(BaseController.class);
+}
